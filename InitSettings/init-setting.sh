f88dde3cce664a4cb8b1f45287b1a0920ec45ae9
u5-03 <yugo.0322@outlook.com>
2020-01-16 00:43:12 +0900

    Add zsh initial setup

--- InitSettings/init-setting.sh
+++ InitSettings/init-setting.sh
@@ -10,6 +10,11 @@ git clone https://github.com/u5-03/SettingFiles.git
 echo source ~/mac-setup/zsh-config.sh > ~/.zshrc
 source ~/.zshrc
 
+# zshの設定
+which -a zsh
+sudo -- sh -c 'echo '/usr/local/bin/zsh' >> /etc/shells'
+chsh -s /usr/local/bin/zsh
+
 # Screen captureの保存先を変更
 ccd
 
