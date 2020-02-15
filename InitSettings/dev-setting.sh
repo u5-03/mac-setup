b17137c8737ab15295fef85387fef93c19fbe8a6
u5-03 <yugo.0322@outlook.com>
2020-02-16 00:38:23 +0900

    Add powerline-shell setting

--- InitSettings/dev-setting.sh
+++ InitSettings/dev-setting.sh
@@ -18,3 +18,19 @@ flutter doctor
 
 # Pods
 sudo gem install cocoapods
+
+# Powerline-shell
+# https://qiita.com/tocomi/items/c0127c44eded54b43c11
+
+## Install Powerline-shell
+git clone https://github.com/b-ryan/powerline-shell $HOME/dev-tool/powerline-shell
+cd $HOME/dev-tool/powerline-shell
+sudo python setup.py install
+## Install fonts
+git clone https://github.com/powerline/fonts.gszit --depth=1 $HOME/dev-tool/fonts
+$HOME/dev-tool/fonts/install.sh
+cd $HOME/dev-tool
+cd $HOME
+mkdir $HOME/.config/powerline
+cp $HOME/SettingFiles/Powerline-shell/config.json $HOME/.config/powerline/config.json
+cp $HOME/SettingFiles/Powerline-shell/mytheme.pyc $HOME/dev-tool/powerline-shell/powerline_shell/themes/mytheme.pyc
--- Setting/terminal.sh
+++ Setting/terminal.sh
@@ -27,29 +27,48 @@ setopt no_beep
 # rm *で確認を求める機能を無効化する
 setopt RM_STAR_SILENT
 
-# Terminalのユーザー名を非表示にする
-PROMPT=$'%n [%*] -> %% '
-# 現在のパスを表示(文字を青色に設定)
-RPROMPT="%{${fg[blue]}%}[%~]%{${reset_color}%}"
+# # Terminalのユーザー名を非表示にする
+# PROMPT=$'%n [%*] -> %% '
+# # 現在のパスを表示(文字を青色に設定)
+# RPROMPT="%{${fg[blue]}%}[%~]%{${reset_color}%}"
+
+# https://qiita.com/tocomi/items/c0127c44eded54b43c11
+function powerline_precmd() {
+    PS1="$(powerline-shell --shell zsh $?)
+    $"
+}
+
+function install_powerline_precmd() {
+  for s in "${precmd_functions[@]}"; do
+    if [ "$s" = "powerline_precmd" ]; then
+      return
+    fi
+  done
+  precmd_functions+=(powerline_precmd)
+}
+
+if [ "$TERM" != "linux" ]; then
+    install_powerline_precmd
+fi
 
 # PROMPT変数内で変数参照する
-setopt prompt_subst
-RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
+# setopt prompt_subst
+# RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
 
 # vcsの表示
 ## vcs_infoロード
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git svn hg bzr
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
-zstyle ':vcs_info:*' actionformats '[%b|%a]'
+# zstyle ':vcs_info:*' enable git svn hg bzr
+# zstyle ':vcs_info:*' check-for-changes true
+# zstyle ':vcs_info:*' stagedstr "%F{yellow}!"
+# zstyle ':vcs_info:*' unstagedstr "%F{red}+"
+# zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
+# zstyle ':vcs_info:*' actionformats '[%b|%a]'
 
 # プロンプト表示直前にvcs_info呼び出し(プロンプトが表示されるたびに、実行される関数)
-precmd () {
-    psvar=()
-    LANG=en_US.UTF-8 vcs_info
-    [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
-}
+# precmd () {
+#     psvar=()
+#     LANG=en_US.UTF-8 vcs_info
+#     [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
+# }
