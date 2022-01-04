c4ced50db0fe5b071e5eda4a32189c6c080b6801
yugo.sugiyama <>
2022-01-04 22:25:19 +0900

    Add java setting

--- InitSettings/Dev/flutter-init.sh
+++ InitSettings/Dev/flutter-init.sh
@@ -4,3 +4,7 @@ git clone https://github.com/flutter/flutter.git -b stable $HOME/dev-tool/flutte
 export PATH="$PATH:$HOME/dev-tool/flutter/bin"
 export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
 flutter doctor
+
+#JAVA
+# Ref: https://zenn.dev/satokazur222/articles/66568417b291d8
+sudo ln -sfn $(brew --prefix)/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
